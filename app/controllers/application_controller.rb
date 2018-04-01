59f7c8c1cf369529d2920a03c25584e7e4c120c0
MichaelSmyth <x16143175@student.ncirl.ie>
2018-04-01 20:50:59 +0000

    Update root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  #function hello to didplay hello world
+  def hello
+    render html: "Hello, and welcome to Michaels Sample App"
+  end
 end
