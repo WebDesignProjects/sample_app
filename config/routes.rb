f40d13410b6ce627d4d22bf17769c306e7c33257
MichaelSmyth <x16143175@student.ncirl.ie>
2018-04-02 07:17:00 +0000

    Add a new controller for Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  # For details on the DSL available within this file
+  # see http://guides.rubyonrails.org/routing.html
   
   #Add a root for application_ controller and the function hello
   root  'application#hello'
