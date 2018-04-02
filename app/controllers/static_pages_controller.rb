28a2844242ef1754864988d1d18e2dbf15ed90a0
MichaelSmyth <x16143175@student.ncirl.ie>
2018-04-02 15:49:25 +0000

    Completed setting home and help 3.2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  #This is a plain function but in ruby the function woudnt do anything
+  #However in rails the empty function likes automatically tries to render
+  #a view with the home.html.erb Rails is clever!!!!!!
   def home
   end
 
