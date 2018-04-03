fac79cab3c28fb24e862c93a277e8bb990205527
MichaelSmyth <x16143175@student.ncirl.ie>
2018-04-03 14:08:40 +0000

    Add a full title helper 4.1.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  #Heplers that apply to any page on the site
+  
+  #Add a helper
+  #Add the full title on a per page based
+  def full_title(page_title = '')
+    #Set a base title
+    base_title = "Ruby on Rails Sample App"
+    #Statement if -else
+    if page_title.empty?
+      base_title
+    else
+      #Interpulate page and base as follows
+      # Updated to concatination
+      page_title + " | " + base_title
+    end
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,4 @@
 module StaticPagesHelper
+ #Heplers that apply only to the static pages on the 
+  
 end
